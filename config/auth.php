935dce46d6a0dea429d095d87e5de1cc03aec2dc
Mariam Elkhashab <54447073+mariamkhashab@users.noreply.github.com>
2021-12-20 11:49:06 +0200

    User auth
    kits installations
    User login in and sign up

--- config/auth.php
+++ config/auth.php
@@ -5,7 +5,7 @@ return [
     /*
     |--------------------------------------------------------------------------
     | Authentication Defaults
-    |--------------------------------------------------------------------------
+    |-----------------------------------------------------------------/////////////////////////////////////////////////////////////////---------
     |
     | This option controls the default authentication "guard" and password
     | reset options for your application. You may change these defaults
@@ -14,7 +14,7 @@ return [
     */
 
     'defaults' => [
-        'guard' => 'web',
+        'guard' => 'api',
         'passwords' => 'users',
     ],
 
@@ -40,6 +40,11 @@ return [
             'driver' => 'session',
             'provider' => 'users',
         ],
+        
+        //user api
+        'api' => [
+            'driver' => 'jwt',
+            'provider' => 'users',]
     ],
 
     /*
@@ -64,11 +69,6 @@ return [
             'driver' => 'eloquent',
             'model' => App\Models\User::class,
         ],
-
-        // 'users' => [
-        //     'driver' => 'database',
-        //     'table' => 'users',
-        // ],
     ],
 
     /*
